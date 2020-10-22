f157b9d8e0e8ad8f6039c60efaa9598baa222cd2
Emil Gabrielli <emilgabrielli@gmail.com>
2020-10-22 15:14:44 +0200

    Begun work on the ABM. Just defined the agent class.

--- covid_simulation.py
+++ covid_simulation.py
@@ -1,17 +1,8 @@
-import matplotlib
-matplotlib.use('TkAgg')
-from pylab import *
+# CLASSES
+class Agent:
 
-def initialize():
-    global x
-    pass
-
-def observe():
-    global x
-    pass
-
-def update():
-    global x
-
-from PyCX import pycxsimulator
-pycxsimulator.GUI().start(func=[initialize, observe, update])
+    def __init__():
+        '''
+        Constructor function.
+        '''
+        pass
