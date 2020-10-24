28506dcb864b35d45af010cdba4e45a3db0cfa51
Emil Gabrielli <emilgabrielli@gmail.com>
2020-10-24 02:34:18 +0200

    Begun work on the model
    Created the blueprint for how an agent is created. Implemented gender and a unique identifier.

--- covid_simulation.py
+++ covid_simulation.py
@@ -1,8 +1,35 @@
+# imports
+import random
+
 # CLASSES
 class Agent:
 
-    def __init__():
+    counter = 0
+    agentdict = {}
+
+    def __init__(self):
         '''
         Constructor function.
         '''
-        pass
+        self.gender = self.assign_gender()
+        self.unique_identifier = self.generate_unique_number()
+        self.name = "agent" + str(self.unique_identifier.zfill(4))
+        Agent.agentdict[self.name] = {"gender":self.gender}
+
+
+    def assign_gender(self):
+        return random.choice(["M","F"])
+
+    def generate_unique_number(self):
+        Agent.counter += 1
+        return str(Agent.counter)
+
+    def overview(self):
+        for x,y in Agent.agentdict.items():
+            print(f"{x}:")
+            for x2,y2 in y.items():
+                print(f"\t{x2}:{y2}")
+
+x = Agent()
+y = Agent()
+x.overview()
