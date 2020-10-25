d9fed17961508518b5bbb2b7cc17e264f33dcb5e
Emil Gabrielli <emilgabrielli@gmail.com>
2020-10-25 16:34:47 +0100

    Added the basic attributes. Using only random.choice with no weights to make sure the basics work :)

--- covid_simulation.py
+++ covid_simulation.py
@@ -11,25 +11,32 @@ class Agent:
         '''
         Constructor function.
         '''
-        self.gender = self.assign_gender()
+        self.gender = random.choice(["M","F"])
         self.unique_identifier = self.generate_unique_number()
         self.name = "agent" + str(self.unique_identifier.zfill(4))
-        Agent.agentdict[self.name] = {"gender":self.gender}
-
-
-    def assign_gender(self):
-        return random.choice(["M","F"])
+        self.infected, self.symptoms = self.infected_and_symptoms()
+        self.mask = random.choice([True,False])
+        Agent.agentdict[self.name] = {"gender":self.gender,"infected":self.infected,"symptoms":self.symptoms if self.symptoms else "no","wears mask":"yes" if self.mask else "no"}
 
     def generate_unique_number(self):
         Agent.counter += 1
         return str(Agent.counter)
 
+    def infected_and_symptoms(self):
+        infected = random.choice([True, False])
+        symptoms = []
+        if infected:
+            symptoms.append(random.choice(["fever","dry cough","fatigue","sputum","shortness of breath","sore throat","headache","myalgia"]))
+        return infected, symptoms
+
     def overview(self):
         for x,y in Agent.agentdict.items():
             print(f"{x}:")
             for x2,y2 in y.items():
-                print(f"\t{x2}:{y2}")
+                print(f"   {x2}: {y2}")
+            print("")
 
-x = Agent()
-y = Agent()
+for i in range(6):
+    x = Agent()
+
 x.overview()
